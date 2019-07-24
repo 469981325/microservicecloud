8d57ce056daa0236a234900fc8068c6ad9ea5375
zhaihao <469981325@qq.com>
2019-07-25 00:29:33 +0800

    增加Feign负载均衡

---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/huawei/springcloud/controller/DeptControllerFeign_Consumer.java
@@ -0,0 +1,35 @@
+package com.huawei.springcloud.controller;
+
+import com.huawei.springcloud.entities.Dept;
+import com.huawei.springcloud.service.DeptClientService;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DeptControllerFeign_Consumer {
+
+    @Autowired
+    private DeptClientService deptClientService;
+
+    @RequestMapping(value = "/conusmer/dept/add")
+    public boolean add(Dept dept){
+        return deptClientService.add(dept);
+    }
+
+    @RequestMapping(value = "/conusmer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id){
+        return deptClientService.get(id);
+    }
+
+    @RequestMapping(value = "/consumer/dept/list")
+    public List<Dept> list(){
+        return deptClientService.list();
+    }
+
+
+}
