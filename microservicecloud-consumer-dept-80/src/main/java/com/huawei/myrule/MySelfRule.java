e54e97e6b1f465f23a63772740e25f9c4028b85d
zhaihao <469981325@qq.com>
2019-07-24 23:52:29 +0800

    实现自己的LB

--- microservicecloud-consumer-dept-80/src/main/java/com/huawei/myrule/MySelfRule.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/huawei/myrule/MySelfRule.java
@@ -1,7 +1,6 @@
 package com.huawei.myrule;
 
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +9,6 @@ public class MySelfRule {
 
     @Bean
     public IRule myRule() {
-        return new RandomRule();
+        return new RamdomRule_WJH();
     }
 }
