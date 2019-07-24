1bc6def747116d976e37ae70fc2311aee87304c2
zhaihao <469981325@qq.com>
2019-07-24 22:49:16 +0800

    测试ribbon的负载均衡

--- microservicecloud-consumer-dept-80/src/main/java/com/huawei/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/huawei/springcloud/controller/DeptController_Consumer.java
@@ -30,7 +30,7 @@ public class DeptController_Consumer {
         return restTemplate.getForObject(REST_URL_PREFIX + "dept/get/"+id,  Dept.class);
     }
 
-    @RequestMapping(value = "/conusmer/dept/list")
+    @RequestMapping(value = "/consumer/dept/list")
     public List<Dept> list(){
         return restTemplate.getForObject(REST_URL_PREFIX + "dept/list",  List.class);
     }
---
+++ microservicecloud-provider-dept-8002/src/main/java/com/huawei/springcloud/service/DeptService.java
@@ -0,0 +1,16 @@
+package com.huawei.springcloud.service;
+
+
+
+import com.huawei.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService
+{
+	public boolean add(Dept dept);
+
+	public Dept get(Long id);
+
+	public List<Dept> list();
+}
